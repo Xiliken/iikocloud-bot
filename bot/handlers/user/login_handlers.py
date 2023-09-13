fa09b486074f5f2f0842eadded0910fb90674015
xiliken <glavzaim.dii@gmail.com>
2023-09-13 10:06:29 +0700

    Первая версия готового бота. TODO: Поменять логику авторизации, сейчас многие методы дублируются, что не есть хорошо.

--- bot/handlers/user/login_handlers.py
+++ bot/handlers/user/login_handlers.py
@@ -11,8 +11,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 import bot.mics.iikoapi
 from api.iIkoCloud.enums import TypeRCI
 from api.iIkoCloud.iIkoCloud import IikoCloudAPI
+from api.sms_center import SMSC
 from bot.database.models.User import User
 from bot.fitlers import IsPhoneNumber
+from bot.keyboards import cabinet_main_kb
 from bot.keyboards.reply import cancel_kb, auth_kb
 from bot.mics.helpers.Config import Config
 from bot.states.user.LoginStates import LoginStates
@@ -41,19 +43,24 @@ async def login_step_phone_number(msg: Message, state: FSMContext, session: Asyn
                                    )
 
     if bot.mics.iikoapi.check_user_exists(iiko_user):
-        # Пользователь существует в iko, на всякий случай добавить его в БД и отправить СМС
+        # Пользователь существует в iko, но проверяем на то, что пользователь с таким ID в Telegram не зарегистрирован на другой номер
         sql = await session.execute(select(User).where(User.user_id == msg.from_user.id and User.phone_number != msg.text))
 
         if sql.scalar():
             await msg.answer('Извините, но данный аккаунт Telegram привязан к другому номеру телефона!', reply_markup=auth_kb())
             await state.clear()
             return
-
         else:
-            await session.merge(User(user_id=msg.from_user.id, phone_number=msg.text, is_admin=False))
-            await msg.answer('На указанный номер отправлен код с подтверждением авторизации!')
-
-        #SMSC().send_sms()
+            # Добавляем пользователя в бд
+            try:
+                await session.merge(User(user_id=msg.from_user.id, phone_number=msg.text, is_admin=False))
+                SMSC().send_sms(phones=f'{msg.text}',
+                                message=f'Код для подтверждения авторизации: {str(verification_code)}')
+                await state.set_state(LoginStates.sms_code)
+                await msg.answer(f'Пожалуйста, введите проверочный код, отправленный на номер: +{msg.text}',
+                                 reply_markup=cancel_kb())
+            except Exception as ex:
+                print(ex)
         await session.commit()
     else:
         # Пользователя не существует
@@ -61,3 +68,42 @@ async def login_step_phone_number(msg: Message, state: FSMContext, session: Asyn
         await msg.answer(f'Извините, не удалось найти пользователя с номером +{msg.text}\n\n'
                          f'Пожалуйста, проверьте корректность введенного номера, или зарегистрируйте новый аккаунт!',
                          reply_markup=auth_kb())
+        return
+
+
+@router.message(StateFilter(LoginStates.sms_code), F.text.isdigit())
+async def login_step_sms(msg: Message, state: FSMContext, session: AsyncSession):
+    user_id = msg.from_user.id
+
+    # Получаем текущее количество попыток из базы данных или переменной
+    current_attempts = attempts.get(user_id, MAX_SMS_ATTEMPTS - 1)
+    print(verification_code)
+
+    if msg.text == str(verification_code):
+        # Код верен, выполните необходимые действия
+        state_data = await state.get_data()
+
+        await msg.answer("🟢 Код успешно подтвержден!")
+        attempts[user_id] = None # Сброс количества попыток
+        await session.commit()
+        await msg.answer('Авторизация успешно завершена!', reply_markup=cabinet_main_kb())
+
+        # Сброс состояния
+        await state.clear()
+    else:
+        # Код неверен
+        if current_attempts is not None and int(current_attempts) > 0:
+            await msg.answer(f"🔴 Неверный код. Осталось попыток: {current_attempts}")
+            attempts[user_id] = current_attempts - 1
+        else:
+            await msg.answer("🔴 Вы 3 раза ввели неверный код! Авторизация отменена!", reply_markup=auth_kb())
+            # Сброс количества попыток
+            attempts[user_id] = None
+            await state.clear()
+
+
+# Если введенно что-то некорректное
+@router.message(StateFilter(LoginStates.sms_code))
+async def warning_sms_handler(msg: Message):
+    await msg.answer('Пожалуйста, введите 4х значный код, отправленный на ваш номер, указанный при регистрации!\n\n'
+                     'Если Вы хотите прервать авторизацию - отправьте команду /cancel')
--- bot/keyboards/inline.py
+++ bot/keyboards/inline.py
@@ -1,18 +1,5 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
-
-def contacts_inline_kb() -> InlineKeyboardMarkup:
-    ikb: InlineKeyboardMarkup = InlineKeyboardMarkup(inline_keyboard=
-    [
-        [
-            InlineKeyboardButton(text='Донерс на Мира', callback_data='doners_mira'),
-            InlineKeyboardButton(text='Донерс на Крас.рабе', callback_data='doners_krasrab')
-        ]
-    ])
-
-    return ikb
-
-
 def chat_inline_kb() -> InlineKeyboardMarkup:
     ikb: InlineKeyboardMarkup = InlineKeyboardMarkup(inline_keyboard=
     [
@@ -39,15 +26,15 @@ def contacts_ikb() -> InlineKeyboardMarkup:
     ikb: InlineKeyboardMarkup = InlineKeyboardMarkup(inline_keyboard=
     [
         [
-            InlineKeyboardButton(text='2ГИС (Мира)', web_app=WebAppInfo(url='https://2gis.ru/krasnoyarsk/branches/70000001059365039/firm/70000001070530973/92.879245%2C56.012341?m=92.879243%2C56.01235%2F18')),
-            InlineKeyboardButton(text='2ГИС (Крас.раб)', web_app=WebAppInfo(url='https://2gis.ru/krasnoyarsk/branches/70000001059365039/firm/70000001059365040/92.969173%2C56.01198?m=92.96918%2C56.011972%2F18'))
+            InlineKeyboardButton(text='2ГИС (Мира)', url='https://2gis.ru/krasnoyarsk/branches/70000001059365039/firm/70000001070530973/92.879245%2C56.012341?m=92.879243%2C56.01235%2F18'),
+            InlineKeyboardButton(text='2ГИС (Крас.раб)', url='https://2gis.ru/krasnoyarsk/branches/70000001059365039/firm/70000001059365040/92.969173%2C56.01198?m=92.96918%2C56.011972%2F18')
         ],
         [
-            InlineKeyboardButton(text='Фламп (Мира)', web_app=WebAppInfo(url='https://krasnoyarsk.flamp.ru/firm/doners_restoran_bystrogo_pitaniya-70000001070530973')),
-            InlineKeyboardButton(text='Флами (Крас.раб)', web_app=WebAppInfo(url='https://krasnoyarsk.flamp.ru/firm/doners_restoran_bystrogo_pitaniya-70000001059365040?ysclid=lmg345gopg224761116'))
+            InlineKeyboardButton(text='Фламп (Мира)', url='https://krasnoyarsk.flamp.ru/firm/doners_restoran_bystrogo_pitaniya-70000001070530973'),
+            InlineKeyboardButton(text='Флами (Крас.раб)', url='https://krasnoyarsk.flamp.ru/firm/doners_restoran_bystrogo_pitaniya-70000001059365040?ysclid=lmg345gopg224761116')
         ],
         [
-            InlineKeyboardButton(text='Яндекс', web_app=WebAppInfo(url='https://yandex.ru/profile/102512988612?no-distribution=1&source=wizbiz_new_map_single'))
+            InlineKeyboardButton(text='Яндекс', url='https://yandex.ru/profile/102512988612?no-distribution=1&source=wizbiz_new_map_single')
         ]
     ])
 
